bef2cbe7c9ed839e621a17f6df27079862e0ebff
Serge Harb <me@sergeharb.com>
2020-03-02 01:41:46 +0200

    improve test fields style

--- src/containers/EmployeeIdentity/styles.ts
+++ src/containers/EmployeeIdentity/styles.ts
@@ -23,7 +23,7 @@ export const useStyles = makeStyles((theme: Theme) =>
       paddingLeft: theme.spacing(2),
     },
     textField: {
-      margin: theme.spacing(1),
+      margin: '8px!important',
     },
     avatar: {
       display: 'inline-flex',
