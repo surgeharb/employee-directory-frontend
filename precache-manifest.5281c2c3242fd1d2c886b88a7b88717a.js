d667bd62f9e2ba574042e14a9ab311d844b934d4
Serge Harb <me@sergeharb.com>
2020-03-02 01:41:52 +0200

    build project

---
+++ precache-manifest.5281c2c3242fd1d2c886b88a7b88717a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d7ae396f14ca5c5694e0f1c653c640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6517e65fa4d3780c58e",
+    "url": "/static/css/main.c58a8230.chunk.css"
+  },
+  {
+    "revision": "87ac8388feecb92197be",
+    "url": "/static/js/2.41ae3d92.chunk.js"
+  },
+  {
+    "revision": "be1f4b53d89b6a2270c84d6d349c3dec",
+    "url": "/static/js/2.41ae3d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6517e65fa4d3780c58e",
+    "url": "/static/js/main.0cfe1178.chunk.js"
+  },
+  {
+    "revision": "0dff5f2811b430358c00",
+    "url": "/static/js/runtime-main.a789b9d3.js"
+  },
+  {
+    "revision": "61af74d7aeaed2753f1128124d731e9b",
+    "url": "/static/media/employee1.61af74d7.png"
+  },
+  {
+    "revision": "628417fad7bdd11f157b6e1f8fb725bb",
+    "url": "/static/media/employee2.628417fa.png"
+  },
+  {
+    "revision": "2918610d97659f858bbbb7c226ed6842",
+    "url": "/static/media/employee3.2918610d.png"
+  },
+  {
+    "revision": "ecf0fd3ade17b6ee050b35b5114d7ae9",
+    "url": "/static/media/employee4.ecf0fd3a.png"
+  }
+]);
