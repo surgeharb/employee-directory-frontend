4bf19e2cb03f69668fed4a36519eb72339b59910
Serge Harb <me@sergeharb.com>
2020-03-02 09:13:56 +0200

    update app title

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.971fb5ec812388a421cac267ac93b253.js"
+  "/precache-manifest.1ef004c44f5bd95a78defac6a5882f8c.js"
 );
 
 self.addEventListener('message', (event) => {
